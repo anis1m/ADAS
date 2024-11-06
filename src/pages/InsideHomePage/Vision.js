74927fd29f71bac848e663aa35dea3a6faf023e3
Anis Mahat <66288071+anis1m@users.noreply.github.com>
2024-11-06 13:55:17 +0530

    Vision.js

--- src/pages/InsideHomePage/Vision.js
+++ src/pages/InsideHomePage/Vision.js
@@ -27,6 +27,7 @@ function Vision() {
     };
   }, []);
   return (
+  <section style={{width:"100%", overflow:"hidden"}}>
     <section className="vision" ref={visionRef}>
       <h2>
         <i class="fa-solid fa-suitcase"></i> Our Vision
@@ -113,6 +114,7 @@ function Vision() {
         </aside>
       </div>
     </section>
+  </section>
   );
 }
 
